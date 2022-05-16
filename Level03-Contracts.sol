bff12e6911b65102180cba6736fcc2c7a2b199d7
jmhickman <jon_hickman@proton.me>
2022-05-16 15:00:06 -0500

    Create Level03-Contracts.sol

---
+++ Level03-Contracts.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../../OZ/token/ERC20/IERC20.sol";
+import "../../OZ/utils/Address.sol";
+import "../../OZ/security/ReentrancyGuard.sol";
+
+/**
+ * @title TrusterLenderPool
+ * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
+ */
+contract TrusterLenderPool is ReentrancyGuard {
+
+    using Address for address;
+
+    IERC20 public immutable damnValuableToken;
+
+    constructor (address tokenAddress) {
+        damnValuableToken = IERC20(tokenAddress);
+    }
+
+    function flashLoan(
+        uint256 borrowAmount,
+        address borrower,
+        address target,
+        bytes calldata data
+    )
+        external
+        nonReentrant
+    {
+        uint256 balanceBefore = damnValuableToken.balanceOf(address(this));
+        require(balanceBefore >= borrowAmount, "Not enough tokens in pool");
+        
+        damnValuableToken.transfer(borrower, borrowAmount);
+        target.functionCall(data);
+
+        uint256 balanceAfter = damnValuableToken.balanceOf(address(this));
+        require(balanceAfter >= balanceBefore, "Flash loan hasn't been paid back");
+    }
+
+}
