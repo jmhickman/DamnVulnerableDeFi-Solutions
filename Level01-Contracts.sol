9e3124827fd638db4a4bcfc07aae62893c7a5586
jmhickman <jon_hickman@proton.me>
2022-07-07 14:17:10 -0500

    Create Level01-Contracts.sol

---
+++ Level01-Contracts.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+interface IReceiver {
+    function receiveTokens(address tokenAddress, uint256 amount) external;
+}
+
+/**
+ * @title UnstoppableLender
+ * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
+ */
+contract UnstoppableLender is ReentrancyGuard {
+
+    IERC20 public immutable damnValuableToken;
+    uint256 public poolBalance;
+
+    constructor(address tokenAddress) {
+        require(tokenAddress != address(0), "Token address cannot be zero");
+        damnValuableToken = IERC20(tokenAddress);
+    }
+
+    function depositTokens(uint256 amount) external nonReentrant {
+        require(amount > 0, "Must deposit at least one token");
+        // Transfer token from sender. Sender must have first approved them.
+        damnValuableToken.transferFrom(msg.sender, address(this), amount);
+        poolBalance = poolBalance + amount;
+    }
+
+    function flashLoan(uint256 borrowAmount) external nonReentrant {
+        require(borrowAmount > 0, "Must borrow at least one token");
+
+        uint256 balanceBefore = damnValuableToken.balanceOf(address(this));
+        require(balanceBefore >= borrowAmount, "Not enough tokens in pool");
+
+        // Ensured by the protocol via the `depositTokens` function
+        assert(poolBalance == balanceBefore);
+        
+        damnValuableToken.transfer(msg.sender, borrowAmount);
+        
+        IReceiver(msg.sender).receiveTokens(address(damnValuableToken), borrowAmount);
+        
+        uint256 balanceAfter = damnValuableToken.balanceOf(address(this));
+        require(balanceAfter >= balanceBefore, "Flash loan hasn't been paid back");
+    }
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../unstoppable/UnstoppableLender.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title ReceiverUnstoppable
+ * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
+ */
+contract ReceiverUnstoppable {
+
+    UnstoppableLender private immutable pool;
+    address private immutable owner;
+
+    constructor(address poolAddress) {
+        pool = UnstoppableLender(poolAddress);
+        owner = msg.sender;
+    }
+
+    // Pool will call this function during the flash loan
+    function receiveTokens(address tokenAddress, uint256 amount) external {
+        require(msg.sender == address(pool), "Sender must be pool");
+        // Return all tokens to the pool
+        require(IERC20(tokenAddress).transfer(msg.sender, amount), "Transfer of tokens failed");
+    }
+
+    function executeFlashLoan(uint256 amount) external {
+        require(msg.sender == owner, "Only owner can execute flash loan");
+        pool.flashLoan(amount);
+    }
+}
+
+/**
+Forgot to go back and add this. Just did it in JS instead of Web3.fs. 
+The short version is that the contract only performs its accounting inside its
+depositTokens function. It doesn't account for other ways for its balance of
+DVT to change. 
+
+Because of the `assert` at line 40, desynchronizing the balance from the actual
+balance is enough to lock the contract unless the deployer had pre-approved an
+allowance to an address under its control. In that event, it could `transferFrom`
+with a precise amount and unlock the contract.
+*/
